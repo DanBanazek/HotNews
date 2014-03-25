f12e609b7b8f8e09e3e9d3b23ffdd00933e6cd15
Dan Banazek <dbanazek@bestcase.com>
2014-03-25 07:54:14 -0500

    Finished tightening up the user interface and added a WebAPI to provide an alternate delivery method for the data

--- BestCaseHotNews.Tests/Controllers/HomeControllerTest.cs
+++ BestCaseHotNews.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,8 @@ namespace BestCaseHotNews.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.AreEqual("Welcome to the new Best Case Hot News!", result.ViewBag.Message);
+
         }
 
         [TestMethod]
